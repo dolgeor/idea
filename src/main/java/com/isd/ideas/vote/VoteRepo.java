87735757b66ad73188b9eb2760d6502a2e0d3f3f
dolgeor <dolgeor@yandex.ru>
2017-12-07 18:46:11 +0200

    idea

--- src/main/java/com/isd/ideas/vote/VoteRepo.java
+++
@@ -1,9 +0,0 @@
-package com.isd.ideas.vote;
-
-import java.sql.Date;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VoteRepo extends CrudRepository<Vote, Long>{
-        Vote findByVoteDate(Date voteDate);
-        
-}
